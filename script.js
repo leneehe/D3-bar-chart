fe947d807bd3707c791b1e204ace32e7139b66c0
Lena <fatmeow@gmail.com>
2018-03-07 07:15:56 -0500

    Using chaining method to ref selections and change elements

--- script.js
+++ script.js
@@ -3,7 +3,16 @@ document.addEventListener("DOMContentLoaded", function(e) {
   // div.innerHTML = "Hello World!";
   // document.body.appendChild(div);
 
-  var section = d3.select("body");
+  //Use chaining method
+  var section = d3.select("body").style("color", "black").style("background-color", "orange");
+
+  section.append("div")
+  .html("First!");
+
+  section.append("div")
+  .html("Second.");
+  // section.style("color", "white");
+  // section.style("background-color", "black");
   var div = section.append("div");
   div.html("Hello, world!");
 });
