aa33b6ef01ae176903c4124e28ebff381a26ac3f
Lena <fatmeow@gmail.com>
2018-03-16 17:44:20 -0400

    Code chart automatically using D3 V4 script

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ document.addEventListener("DOMContentLoaded", function(e) {
 
   // Use Linear scale to make dependencies explicit - eliminate the magic number 10
   //D3’s scales specify a mapping from data space (domain) to display space (range)
-  var x = d3.scale.linear().domain([0, d3.max(data)]).range([0, 420]);
+  var x = d3.scaleLinear().domain([0, d3.max(data)]).range([0, 420]);
 
   //data join -> define the selection to join data -> join the data to selection using selection.data -> enter selection reppresents new data (update and exit are empty) -> instatiate element by appending div to enter section -> set new bar as multiple of 10 and compute style. -> set text content of each bar
   d3.select(".chart").selectAll("div").data(data).enter().append("div").style("width", function(d) { return x(d) + "px" }).text(function(d) {return '$'+ d; });
